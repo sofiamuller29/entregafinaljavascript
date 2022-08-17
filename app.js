09fc3a1cd0974a17b1a479e69f96844a0bb449f8
Sofia <mariasofia.muller@gmail.com>
2022-08-17 09:03:45 -0400

    commit 2

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('api.json')
+        const data = await res.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+} 
+
+
+
